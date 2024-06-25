27c2c1d67b9ed98ec78ee3550263ffd87f6f23bd
Erjan <tasmatoverzan05@gmail.com>
2024-06-25 12:40:48 +0600

    Task #1 Completed. Since the last commit, I moved all the code for processing/receiving/returning to a separate class, and I also remade the code to match the one that the mentor did in the lesson so that several clients could connect

--- EchoServer/EchoServer.java
+++ EchoServer/EchoServer.java
@@ -3,13 +3,13 @@ package month6.Echo.EchoServer;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class EchoServer {
+  private final ExecutorService pool = Executors.newCachedThreadPool();
   private final int port;
 
-  private EchoServer(int port) {
+  public EchoServer(int port) {
     this.port = port;
   }
 
@@ -17,14 +17,12 @@ public class EchoServer {
     return new EchoServer(port);
   }
 
+
   public void run() {
     try (var server = new ServerSocket(port)) {
-//      try (var clientSocket = server.accept()) {
-//        handle(clientSocket);
-//      }
       while (!server.isClosed()){
         Socket socket = server.accept();
-        handle(socket);
+        pool.submit(() ->handle(socket));
       }
     } catch (IOException e) {
       var formatMsg = "Вероятнее всего порт %s занят.%n";
@@ -35,12 +33,7 @@ public class EchoServer {
 
   private void handle(Socket clientSocket){
     InformationProcessor p = new InformationProcessor();
-    try {
       p.handle(clientSocket);
-    }catch (IOException e){
-      System.out.println("Ошибка при подключении");
-      e.printStackTrace();
-    }
   }
 
 }
--- EchoServer/InformationProcessor.java
+++ EchoServer/InformationProcessor.java
@@ -1,32 +1,59 @@
 package month6.Echo.EchoServer;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class InformationProcessor {
-    public void handle(Socket socket) throws IOException {
-        var input = socket.getInputStream();
-        var isr = new InputStreamReader(input, "UTF-8");
-        var output = socket.getOutputStream();
-        var writer = new PrintWriter(new OutputStreamWriter(output, "UTF-8"), true);
 
-        try (var scanner = new Scanner(isr)) {
+    private boolean isEmptyMsg(String message){
+        return message == null || message.isBlank();
+    }
+
+    private void sendResponse(String response, Writer writer) throws IOException {
+        writer.write(response);
+        writer.write(System.lineSeparator());
+        writer.flush();
+    }
+
+    private Scanner getReader(Socket socket) throws IOException {
+        InputStream input = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(input, "UTF-8");
+        return new Scanner(isr);
+    }
+
+    private PrintWriter getWriter(Socket socket)throws IOException{
+        OutputStream  output = socket.getOutputStream();
+        var writer = new PrintWriter(new OutputStreamWriter(output, "UTF-8"), true);
+        return new PrintWriter(writer);
+    }
+
+    private boolean isQuitMsg(String message){
+            return "bye".equalsIgnoreCase(message);
+    }
+
+    public void handle(Socket socket)  {
+        System.out.printf("Подключен клиент: %s%n",socket.getPort());
+
+        try(socket;
+            Scanner reader = getReader(socket);
+            PrintWriter writer = getWriter(socket)) {
+            sendResponse("Привет"+ socket.getPort(),writer);
             while (true) {
-                var message = scanner.nextLine().strip();
-                System.out.printf("Got: %s%n", message);
+                String message = reader.nextLine().strip();
                 writer.println(message);
-                if (message.toLowerCase().equals("bye")) {
-                    System.out.println("Bye bye");
+                if (isEmptyMsg(message)|| isQuitMsg(message)) {
                     return;
                 }
+                sendResponse(message.toUpperCase(),writer);
             }
+
         } catch (NoSuchElementException ex) {
             System.out.println("Client dropped connection");
+        }catch (IOException e){
+            e.printStackTrace();
         }
+        System.out.printf("клиент отключен: %s%n",socket.getPort() );
     }
 }
